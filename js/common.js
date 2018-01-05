bc673aa9ee8e4ab5987801e29a1c981912e4fa48
Yuriy <prezlyata.yuriy@gmail.com>
2018-01-05 16:58:46 +0200

    add cycle

--- js/common.js
+++ js/common.js
@@ -8,6 +8,7 @@ var Product = function(name, price, weight) {
 };
 
 // var storeList = document.getElementById('store_list');
+
 //
 // storeList.addEventListener('click', function(e){
 //     var el = e.target;
@@ -19,21 +20,17 @@ var Product = function(name, price, weight) {
 
 var products = [];
 
-function buyProduct() {
-    var storeList = document.getElementById('store_list');
-    storeList.addEventListener('click', function(e){
-        var el = e.target;
-        while (el && el.tagName !== 'LI') {
-            el = el.parentNode
-        }
-        var name = document.getElementsByClassName('item_name')[0].innerHTML;
-        var price = document.getElementsByClassName('item_price')[0].innerHTML;
-        price = parseFloat(price);
-        var weight = document.getElementsByClassName('item_weight')[0].value;
-        weight = parseFloat(weight);
-        var newProduct = new Product(name, price, weight);
-        products.push(newProduct);
-        console.log(products);
-    }, false);
+var listLength = document.getElementsByTagName('li').length;
 
+function buyProduct() {
+   for(var i = 0; i < listLength; i++) {
+       var name = document.getElementsByClassName('item_name')[i].innerHTML;
+       var price = document.getElementsByClassName('item_price')[i].innerHTML;
+       price = parseFloat(price);
+       var weight = document.getElementsByClassName('item_weight')[i].value;
+       weight = parseFloat(weight);
+       var newProduct = new Product(name, price, weight);
+       products.push(newProduct);
+       console.log(products);
+   }
 };
